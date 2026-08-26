62a36c444046cbd4b1beecabc2f823353c86feae
Mike Krüger <mikkrg@microsoft.com>
2026-08-26 07:05:46 +0200

    Serialize help console output test

--- CosmosDBShell.Tests/HelpCommandVerificationTests.cs
+++ CosmosDBShell.Tests/HelpCommandVerificationTests.cs
@@ -7,8 +7,10 @@ namespace CosmosShell.Tests;
 using System.Text;
 using Azure.Data.Cosmos.Shell.Commands;
 using Azure.Data.Cosmos.Shell.Core;
+using CosmosShell.Tests.CommandTests;
 using Xunit;
 
+[Collection(WelcomeConsoleTestCollection.Name)]
 public class HelpCommandVerificationTests
 {
     [Fact]
